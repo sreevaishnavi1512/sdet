96c2f31135c5fda7522b7934a7f9abc94ce02b17
user <user@DESKTOP-7HJQV2M>
2022-03-24 15:55:20 +0530

    push

--- com.crm.Demo_1/src/test/java/Contact/addContactTest.java
+++ com.crm.Demo_1/src/test/java/Contact/addContactTest.java
@@ -5,10 +5,15 @@ import org.testng.annotations.Test;
 public class addContactTest {
 	@Test
 	public void addContact() {
-		System.out.println("addContact");
+		System.out.println("addContact");}
 		
 		
-		
+		@Test
+		public void addContact2() {
+			System.out.println("addContact");
+			
+			
+			
 	
 		
 		
